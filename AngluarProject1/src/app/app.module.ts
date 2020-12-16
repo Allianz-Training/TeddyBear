84150a8050a47a9b9efec1bbe689d12e11c1023f
pteddy9087 <74392102+pteddy9087@users.noreply.github.com>
2020-12-16 21:28:16 +0700

    12/16/2020

--- AngluarProject1/src/app/app.module.ts
+++ AngluarProject1/src/app/app.module.ts
@@ -7,10 +7,58 @@ import { BackendService } from './backend.service';
 import { FormsModule } from '@angular/forms';
 import { CatalogModule } from './catalog/catalog.module';
 import { HomeModule } from './home/home.module';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductListComponent } from './catalog/product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'products',
+    component: ProductListComponent,
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, FormsModule, CatalogModule, HomeModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CatalogModule,
+    HomeModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [BackendService],
   bootstrap: [AppComponent],
 })
